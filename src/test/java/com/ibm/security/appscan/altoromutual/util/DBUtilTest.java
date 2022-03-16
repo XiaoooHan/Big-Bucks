483706e2b1830d2d869a29bda4df50281a5cf66f
xh109 <xh109@duke.edu>
2022-03-15 22:03:46 -0400

    week1-Create testcases for DBUtil

---
+++ src/test/java/com/ibm/security/appscan/altoromutual/util/DBUtilTest.java
@@ -0,0 +1,16 @@
+package com.ibm.security.appscan.altoromutual.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DBUtilTest {
+
+    @Test
+    void isDuplicateID() {
+    }
+
+    @Test
+    void signUp() {
+    }
+}
