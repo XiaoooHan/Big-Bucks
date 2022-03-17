1a3cc3235d90b0ca5da1c6dc0272f58ceeb58dba
jx118 <jx118@duke.edu>
2022-03-17 15:03:27 -0400

    testcase1 for isDuplicated method--true edition

--- src/test/java/com/ibm/security/appscan/altoromutual/util/DBUtilTest.java
+++ src/test/java/com/ibm/security/appscan/altoromutual/util/DBUtilTest.java
@@ -2,12 +2,22 @@ package com.ibm.security.appscan.altoromutual.util;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.SQLException;
+
+import static com.ibm.security.appscan.altoromutual.util.DBUtil.isDuplicateID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class DBUtilTest {
 
     @Test
-    void isDuplicateID() {
+    void isDuplicateID_test() throws SQLException {
+        boolean expect1 = true;
+        String name = "christinax"; //name already saved inside the data set
+        boolean test = isDuplicateID(name);
+
+        assertEquals(expect1,test);
+
     }
 
     @Test
