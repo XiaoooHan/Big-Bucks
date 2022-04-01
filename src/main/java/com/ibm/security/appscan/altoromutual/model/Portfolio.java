4b4287b037194980cdf15ef4d94e272ac3fb6666
xh109 <xh109@duke.edu>
2022-04-01 01:05:24 -0400

    #3 Add Portfolio Model

---
+++ src/main/java/com/ibm/security/appscan/altoromutual/model/Portfolio.java
@@ -0,0 +1,61 @@
+package com.ibm.security.appscan.altoromutual.model;
+
+public class Portfolio {
+
+    private int stockId;
+    private long accountId;
+    private String symbol;
+    private int amount;
+    private double price;
+    private double value;
+
+    public Portfolio(int stockId, long accountId, String symbol, int amount, double price) {
+        this.accountId = accountId;
+        this.amount = amount;
+        this.symbol = symbol;
+        this.stockId = stockId;
+        this.price = price;
+        this.value = price*(double)amount;
+    }
+
+    public int getStockId() {
+        return stockId;
+    }
+
+    public long getAccountId() {
+        return accountId;
+    }
+
+    public int getAmount() {return amount;}
+
+    public String getSymbol(){return symbol;}
+
+    public Double getPrice(){return price;}
+
+    public double getValue() { return value; }
+
+    public void setAmount(int amount){
+        this.amount = amount;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void setValue(double value){
+        this.value = value;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Portofolio{" +
+                "stockId=" + stockId +
+                ", accountId=" + accountId +
+                ", stockSymbol=" + symbol +
+                ", amount=" + amount +
+                ", price=" + price +
+                ", value=" + value +
+                '}';
+    }
+}
